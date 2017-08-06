411b2ce0582fc25cf0180722eb8e6335b7169775
Cherise Knox <cheriseknox@Cherises-MacBook-Pro.local>
2017-08-06 17:10:24 -0400

    homework

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,41 +3,50 @@ var friends = require("../data/friends.js");
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
 		res.json(friends);
-	}); 
+	})
 
 	app.post("/api/friends", function(req, res) {
 
-		var perfectMatch = {
-			name: "", 
-			image: "", 
-			matchDifference: 1000
-		}; 
+	var newMatch = req.body;
 
-		var userData = req.body; 
-		var userName = userData.name; 
-		var userImage = userData.image; 
-		var userScores = userData.scores; 
-
-		var totalDifference = 0;  
-
-		for(var i = 0; i < [friends].length-1; i++) {
-			console.log(friends[i].name);
-			totalDifference = 0; 
-
-			for(var j = 0; j < 10; j++) {
-				totalDifference += Math.abs(parseInt(userScores[j]) - 
-					parseInt(friends[i].scores[j])); 
-
-				if (totalDifference <= perfectMatch.friendDifference) {
-					perfectMatch.name = friends[i].name; 
-					perfectMatch.photo = friends[i].photo; 
-					perfectMatch.matchDifference = totalDifference;
-				}
+		for(var i = 0; i < newMatch.scores.length; i++) {
+			if(newMatch.scores[i] == "1 (Strongly Disagree)") {
+				newMatch.scores[i] = 1;
+			} else if(newMatch.scores[i] == "5 (Strongly Agree)") {
+				newFriend.scores[i] = 5;
+			} else {
+				newFriend.scores[i] = parseInt(newFriend.scores[i]);
 			}
-		} 
-	friends.push(userData); 
+		}
 
-	res.json(perfectMatch); 
+		var differencesArray = [];
 
-	});
+		for(var i = 0; i < friendData.length; i++) {
+
+			var compare = friendData[i];
+			var totalDifference = 0;
+			
+			for(var k = 0; k < compare.scores.length; k++) {
+				var differenceOneScore = Math.abs(compare.scores[k] - newFriend.scores[k]);
+				totalDifference += differenceOneScore;
+			}
+
+			differencesArray[i] = totalDifference;
+		}
+
+		var newFriendNum = differencesArray[0];
+		var newFriendIndex = 0;
+
+		for(var i = 1; i < differencesArray.length; i++) {
+			if(differencesArray[i] < newFriendNum) {
+				newFriendNum = differencesArray[i];
+				newFriendIndex = i;
+			}
+		}
+
+		friends.push(newMatch);
+
+		res.json(friends[newFriendIndex]);
+	})
 }
+
